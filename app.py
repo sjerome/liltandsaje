69e68715211d89c254e755389bc190353e1057e3
Aaron <ahimelman@gmail.com>
2014-09-18 20:27:35 -0400

    added get_color

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from datetime import date
+import hashlib
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	get_color()
+	return "Hello World!"
+
+def get_color():
+	today = date.today()
+	hashy = hashlib.sha1()
+	hashy.update(today)
+	print hashy 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
+
